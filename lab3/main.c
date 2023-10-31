b5173f31f776832c9eb3a599695d2eaa8f3e90e2
eglogger <144871990+eglogger@users.noreply.github.com>
2023-11-01 01:40:28 +0700

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -3,44 +3,44 @@
 #include <malloc.h>
 
 #define MAX_LEN (16 + 2)
+#define ALPHABET_SIZE 256
 
 // Creating table of symbols with information about shifting.
-int *preprocess(char *pattern, int startIndex, int endIndex) {
+int *preprocess(char *pattern, int patternLen) {
 
-    int *table = malloc((endIndex - startIndex + 1) * sizeof(int));
+    int *table = malloc(ALPHABET_SIZE * sizeof(int));
 
-    for (int i = 0; i < endIndex - startIndex + 1; i++)
-        table[i] = strlen(pattern);
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        table[i] = patternLen;
 
     for (int i = 0; i < strlen(pattern) - 1; i++)
-        table[pattern[i] - startIndex] = strlen(pattern) - i - 1;
+        table[pattern[i]] = patternLen - i - 1;
 
     return table;
 }
 
 // Using Boyer-Moore algorithm.
-void BoyerMoore(char* text, char* pattern) {
+void BoyerMoore(char* text, char* pattern, int textLen, int patternLen) {
 
-    int i = strlen(pattern) - 1;
-    int start = ' ';
-    int end = '~';
-    int* table = preprocess(pattern, start, end);
+    int i = patternLen - 1;
+    int* table = preprocess(pattern, patternLen);
 
-    while (i < strlen(text)) {
+    while (i < textLen) {
 
         printf("%d ", i + 1);
 
         int j = 0;
 
-        while (j < strlen(pattern) && text[i - j] == pattern[strlen(pattern) - 1 - j]) {
+        while ((j < patternLen) &&
+              (text[i - j] == pattern[patternLen - 1 - j])) {
 
             j++;
 
-            if (j != strlen(pattern))
+            if (j != patternLen)
                 printf("%d ", i - j + 1);
         }
 
-        i += table[text[i] - start];
+        i += table[text[i]];
     }
 
     free(table);
@@ -51,13 +51,18 @@ int main() {
     char pattern[MAX_LEN];
     char text[1000];
 
+    int textLen, patternLen;
+
     fgets(pattern, MAX_LEN, stdin);
     fgets(text, 1000, stdin);
 
     pattern[strcspn(pattern, "\n")] = 0;
     text[strcspn(text, "\n")] = 0;
 
-    BoyerMoore(text, pattern);
+    textLen = strlen(text);
+    patternLen = strlen(pattern);
+
+    BoyerMoore(text, pattern, textLen, patternLen);
 
     return 0;
 }
