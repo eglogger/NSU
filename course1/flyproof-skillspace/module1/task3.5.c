3e3f8f37f80a1fa36a69bdb487e776dd908daecb
Mikhail Agilov <144871990+eglogger@users.noreply.github.com>
2023-12-20 19:51:08 +0700

    Update task3.5.c

--- course1/flyproof-skillspace/module1/task3.5.c
+++ course1/flyproof-skillspace/module1/task3.5.c
@@ -5,7 +5,7 @@ int main() {
 
     double sum = 0;
 
-    for(int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         sum += M_PI / 10 * sin(M_PI / 10 * i + M_PI / 20);
 
     printf("%f", sum);
