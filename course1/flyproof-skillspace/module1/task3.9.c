f13333370cb7d5e0be93e55f59c9234ddd5dc40e
Mikhail Agilov <144871990+eglogger@users.noreply.github.com>
2023-12-20 19:50:20 +0700

    Update task3.9.c

--- course1/flyproof-skillspace/module1/task3.9.c
+++ course1/flyproof-skillspace/module1/task3.9.c
@@ -5,9 +5,9 @@ int main() {
 
     float p, p0 = 1.29, z = 1.25 * 0.0001, e = 2.71;
 
-    for(int h = 0; h < 10000; h++) {
+    for (int h = 0; h < 10000; h++) {
         p = p0 * pow(e, -h * z);
-        if( p < 1) {
+        if (p < 1) {
             printf("%d", h);
             return 0;
         }
